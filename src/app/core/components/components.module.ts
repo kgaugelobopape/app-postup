05485a31f4437f534634f462c272db1bde13c95c
Kgaugelo Bopape <kgaugelo20@gmail.com>
2017-06-12 21:59:31 +0200

    registres loader ang navigation component

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [NavigationComponent, LoaderComponent]
 })
 export class ComponentsModule { }
