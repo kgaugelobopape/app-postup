8df1e52d018671e48c0650ad92e762c72e22276b
Kgaugelo Bopape <kgaugelo20@gmail.com>
2017-06-13 21:07:22 +0200

    added service urls

--- src/core/core.service.ts
+++ src/core/core.service.ts
@@ -2,25 +2,14 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class CoreService {
-  
-  SERVICE_BASE_URI = 'GET https://newsapi.org/v1';
+  baseUrl = 'https://newsapi.org/v1';
+  articles: string;
+  sources: string;
+  apiKey: string;
   
   constructor() {
-  }
-  
-  public get BASE_URI() {
-    return this.SERVICE_BASE_URI;
-  }
-  
-  public get ARTICLES_URL() {
-    return this.SERVICE_BASE_URI + '/article';
-  }
-  
-  public get SOURCE_URL() {
-    return this.SERVICE_BASE_URI + '/source';
-  }
-  
-  public get API_KEY() {
-    return 'dcf4480f211c48fa943c3b28ba798ef7';
+    this.articles = this.baseUrl + '/articles';
+    this.sources = this.baseUrl + '/source';
+    this.apiKey = 'dcf4480f211c48fa943c3b28ba798ef7';
   }
 }
