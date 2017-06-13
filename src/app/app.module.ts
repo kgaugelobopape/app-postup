90c6613920c3b5fefb672106cb5c6bf80f673a13
Kgaugelo Bopape <kgaugelo20@gmail.com>
2017-06-13 09:14:54 +0200

    moved core folder to src

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
-import {CoreModule} from './core/core.module';
-import {ComponentsModule} from './core/components/components.module';
+import {CoreModule} from '../core/core.module';
+import {ComponentsModule} from '../core/components/components.module';
 
 @NgModule({
   declarations: [
