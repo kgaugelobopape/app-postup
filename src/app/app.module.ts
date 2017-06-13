3a07993aebea967ac45b2887a9782f51ab7b9140
Kgaugelo Bopape <kgaugelo20@gmail.com>
2017-06-13 22:26:19 +0200

    added dashboard module with component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {CoreModule} from '../core/core.module';
+import {DashboardModue} from '../components/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import {CoreModule} from '../core/core.module';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    CoreModule,
+    DashboardModue
   ],
   bootstrap: [AppComponent]
 })
--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -4,10 +4,12 @@ import {CoreService} from './core.service';
 import {NavigationComponent} from './components/navigation/navigation.component';
 import {LoaderComponent} from './components/loader/loader.component';
 import {ContainerComponent} from './components/container/container.component';
+import {routes} from './core.router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    routes
   ],
   declarations: [NavigationComponent, LoaderComponent, ContainerComponent],
   providers: [CoreService],
