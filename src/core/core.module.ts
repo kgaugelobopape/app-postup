441f1cb543c15f929ae2901983dcbbb4a4c500e0
Kgaugelo Bopape <kgaugelo20@gmail.com>
2017-06-13 21:25:41 +0200

    Added container component

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -3,14 +3,15 @@ import {CommonModule} from '@angular/common';
 import {CoreService} from './core.service';
 import {NavigationComponent} from './components/navigation/navigation.component';
 import {LoaderComponent} from './components/loader/loader.component';
+import {ContainerComponent} from './components/container/container.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [NavigationComponent, LoaderComponent],
+  declarations: [NavigationComponent, LoaderComponent, ContainerComponent],
   providers: [CoreService],
-  exports : [NavigationComponent, LoaderComponent]
+  exports : [NavigationComponent, LoaderComponent, ContainerComponent]
 })
 export class CoreModule {
 }
