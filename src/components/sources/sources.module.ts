2bc8077d6f547414c69e325a8cded73627d8b795
Kgaugelo Bopape <kgaugelo20@gmail.com>
2017-06-16 10:49:18 +0200

    added  sources component

---
+++ src/components/sources/sources.module.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by kgaugelobopape on 2017/06/16.
+ */
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SourcesComponent} from './sources.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [SourcesComponent],
+  exports: [SourcesComponent]
+})
+export class SourcesModule {
+}
