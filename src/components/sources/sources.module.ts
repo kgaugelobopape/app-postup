14a4281344b5c0d06ef623d3f8cc12987f3e55ee
Kgaugelo Bopape <kgaugelo20@gmail.com>
2017-06-18 22:16:32 +0200

    updated sources component path

--- src/components/sources/sources.module.ts
+++ src/components/sources/sources.module.ts
@@ -1,14 +1,3 @@
 /**
- * Created by kgaugelobopape on 2017/06/16.
+ * Created by kgaugelobopape on 2017/06/18.
  */
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {SourcesComponent} from './sources.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [SourcesComponent],
-  exports: [SourcesComponent]
-})
-export class SourcesModule {
-}
---
+++ src/core/components/sources/sources.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {CoreService} from '../../core/core.service';
+
+@Component({
+  selector: 'app-sources',
+  templateUrl: './sources.component.html',
+  styleUrls: ['./sources.component.css']
+})
+export class SourcesComponent implements OnInit {
+  title = 'PostUp';
+  sources: any = [];
+  queryText: string;
+  
+  constructor(private _service: CoreService) {}
+  
+  ngOnInit() {
+    this._service.getSources().subscribe((response) => {
+      this.sources = response.sources;
+    });
+  }
+}
